c38522727dfb49eefcf41cd76dd880a6ce5e32c9
Haebin <joker7011@naver.com>
2024-02-12 19:18:41 +0900

    feat: Initial Commit

---
+++ src/main/java/com/partybbangbbang/PartybbangbbangApplication.java
@@ -0,0 +1,13 @@
+package com.partybbangbbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartybbangbbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartybbangbbangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/partybbangbbang/PartybbangbbangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partybbangbbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartybbangbbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
